bdae71bd075446e31e7ed5494219b4447c5393c4
renaud archer fortin <renaud.archerf@gmail.com>
2021-01-06 14:37:35 -0500

    base without redux done

---
+++ src/containers/city.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const City = (props) => {
+  return (
+    <li className="list-group-item">{props.city.name}</li>
+  );
+};
+
+export default City;
---
+++ src/containers/city_list.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import City from "./city";
+
+const CityList = (props) => {
+  const cityArr = props.cities;
+  const cities = cityArr.map((city) => {
+    return (
+      <City key={city.name} city={city} />
+    );
+  });
+  return (
+    <ul className="list-group cities">
+      {cities}
+    </ul>
+  );
+};
+
+
+export default CityList;
